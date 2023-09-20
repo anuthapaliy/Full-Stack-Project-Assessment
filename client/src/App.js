b1cb6afbeeff921f2b5c8eb5c80b965229500df3
anuthapaliy <113211321+anuthapaliy@users.noreply.github.com>
2023-09-20 10:42:40 +0100

    on process of 300-review

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/videos")
+    .then((response) => response.json())
+    .then((data) => {
+      setVideos(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:",error);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
+        <div>
+          <h2>Video List</h2>
+          <ul>
+            {videos.map((video) => (
+              <li key={video.id}>
+                <strong>{video.title}</strong>
+                <br />
+                Rating: {video.rating}
+                
+              </li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const dotenv = require('dotenv');
+const { Pool } = require("pg");
+const { response } = require("express");
+const port = process.env.PORT || 8000;
+
+dotenv.config();
+
+// create a postgreSQL Database connection
+
+const db = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -13,3 +29,80 @@ app.get("/", (req, res) => {
   // Delete this line after you've confirmed your server is running
   res.send({ express: "Your Backend Service is Running" });
 });
+// Define an endpoint to fetch videos
+app.get("/videos", async (request, response) => {
+  try {
+    const query = 'SELECt * From videos';
+
+    const { rows } = await db.query(query);
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: 'An error occured' });
+  }
+});
+
+// Add a new video to the database
+  app.post("/videos", async (request, response) => {
+    try {
+      const { title, url, rating } = request.body;
+
+      const query = 'INSERT INTO videos (title, url, rating) VALUES ($1, $2, $3)';
+      await db.query(query, [title, url, rating]);
+
+      response.status(201).json({ message: "Video added successfully" });
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: 'An error occured' });
+    }
+  });
+
+  // Update an existing video by ID
+  app.put("/videos/:id", async (request, response) => {
+    try {
+      const { title, url, rating } = request.body;
+      const videoId = request.params.id;
+
+      const query = 'UPDATE videos SET title = $1, url = $2, rating = $3 WHERE id = $4';
+      await db.query(query, [title, url, rating, videoId]);
+
+      response.status(200).json({ message: "Video updated successfully" });
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: 'An error occured' });
+    }
+  });
+
+  // Delete a video by ID
+  app.delete("/videos/:id", async (request, response) => {
+    try {
+      const videoId = request.params.id;
+
+      const query = 'Delete FROM videos WHERE id = $1';
+      await db.query(query, [videoId]);
+
+      response.status(200).json({ message: "Video deleted successfully" });
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: 'An error occured' });
+    }
+  });
+
+  // Get a video by ID
+  app.get("/videos/:id", async (request, response) => {
+    try {
+      const videoId = request.params.id;
+
+      const query = 'SELECT * FROM videos WHERE id = $1';
+      const { rows } = await db.query(query, [videoId]);
+
+      if (rows.length === 0) {
+        response.status(404).json({ message: "Video not found" });
+      } else {
+        response.status(200).json(rows[0]);
+      }
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: 'An error occured' });
+    }
+  });
